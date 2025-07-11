f0a7bd3f158746b421d32905b1334c8c48bbe63e
MohammedSami01 <itzsamii6980@gmail.com>
2025-07-12 01:28:36 +0530

    Initial commit

---
+++ client/src/pages/Home.js
@@ -0,0 +1,142 @@
+import React , {useState,useEffect} from 'react'
+import { useSelector , useDispatch } from 'react-redux'
+import DefaultLayout from '../components/DefaultLayout'
+import { getAllCars } from '../redux/actions/carsActions'
+import { Col, Row , Divider , DatePicker, Checkbox} from 'antd'
+import {Link} from 'react-router-dom'
+import Spinner from '../components/Spinner';
+import moment from 'moment'
+const {RangePicker} = DatePicker
+function Home() {
+    const {cars} = useSelector(state=>state.carsReducer)
+    const {loading} = useSelector(state=>state.alertsReducer)
+    const [totalCars , setTotalcars] = useState([])
+    const dispatch = useDispatch()
+    
+
+    useEffect(() => {
+        dispatch(getAllCars())
+    }, [])
+
+    useEffect(() => {
+
+        setTotalcars(cars)
+        
+    }, [cars])
+
+
+    function setFilter(values){
+
+        var selectedFrom = moment(values[0] , 'MMM DD yyyy HH:mm')
+        var selectedTo = moment(values[1] , 'MMM DD yyyy HH:mm')
+
+        var temp=[]
+
+        for(var car of cars){
+
+              if(car.bookedTimeSlots.length == 0){
+                  temp.push(car)
+              }
+              else{
+
+                   for(var booking of car.bookedTimeSlots) {
+
+                       if(selectedFrom.isBetween(booking.from , booking.to) ||
+                       selectedTo.isBetween(booking.from , booking.to) || 
+                       moment(booking.from).isBetween(selectedFrom , selectedTo) ||
+                       moment(booking.to).isBetween(selectedFrom , selectedTo)
+                       )
+                       {
+
+                       }
+                       else{
+                           temp.push(car)
+                       }
+
+                   }
+
+              }
+
+        }
+
+
+        setTotalcars(temp)
+
+
+    }
+
+    return (
+        <DefaultLayout>
+
+             <Row className='mt-3' justify='center'>
+                 
+                 <Col lg={20} sm={24} className='d-flex justify-content-left'>
+
+                     <RangePicker showTime={{format: 'HH:mm'}} format='MMM DD yyyy HH:mm' onChange={setFilter}/>
+                 
+                 </Col>
+
+             </Row>
+
+              {loading == true && (<Spinner/>)}
+
+
+              
+              <Row justify='center' gutter={16}>
+
+                   {totalCars.map(car=>{
+                       return <Col lg={5} sm={24} xs={24}>
+                            <div className="car p-2 bs1">
+                               <div style={{ 
+                                 width: '100%', 
+                                 height: '180px', 
+                                 overflow: 'hidden', 
+                                 display: 'flex', 
+                                 alignItems: 'center', 
+                                 justifyContent: 'center',
+                                 backgroundColor: '#f5f5f5',
+                                 borderRadius: '4px'
+                               }}>
+                                 <img 
+                                   src={car.image} 
+                                   style={{
+                                     maxWidth: '100%',
+                                     maxHeight: '100%',
+                                     width: 'auto',
+                                     height: 'auto',
+                                     objectFit: 'contain',
+                                     objectPosition: 'center',
+                                     padding: '10px',
+                                     boxSizing: 'border-box'
+                                   }} 
+                                   alt={car.name}
+                                   onError={(e) => {
+                                     e.target.onerror = null;
+                                     e.target.src = 'https://via.placeholder.com/300x180?text=Image+Not+Available';
+                                   }}
+                                 />
+                               </div>
+
+                               <div className="car-content d-flex align-items-center justify-content-between">
+
+                                    <div className='text-left pl-2'>
+                                        <p>{car.name}</p>
+                                        <p> Rent Per Hour {car.rentPerHour} /-</p>
+                                    </div>
+
+                                    <div>
+                                        <button className="btn1 mr-2"><Link to={`/booking/${car._id}`}>Book Now</Link></button>
+                                    </div>
+
+                               </div>
+                            </div>
+                       </Col>
+                   })}
+
+              </Row>
+
+        </DefaultLayout>
+    )
+}
+
+export default Home
